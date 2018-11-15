f01b7b67b16de7789c412b47536ed9c18c99719c
kami.jami <kami.jami@gmail.com>
2018-11-16 01:28:41 +0330

    Implement Edit Photo in APP
    - Using Cloudinary as a photo storage solution
    - Creating the Photos controller
    - Creating the Photo upload component in Angular
    - Adding and Configuring a 3rd Party File Uploader
    - Adding the Set Main photo functionality to the API and SPA
    - Output properties revisited
    - Adding the main photo to the Nav bar
    - Using BehaviorSubject to add any to any communication to our app
    - Adding the Delete photo function to the API and SPA

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -10,18 +10,25 @@ import { User } from '../_models/user';
 export class UserService {
   baseUrl = environment.apiUrl;
 
-constructor(private Http: HttpClient) { }
+constructor(private http: HttpClient) { }
 
     getUsers(): Observable<User[]> {
-        return this.Http.get<User[]>(this.baseUrl + 'users');
+        return this.http.get<User[]>(this.baseUrl + 'users');
     }
 
     getUser(id): Observable<User> {
-        return this.Http.get<User>(this.baseUrl + 'users/' + id);
+        return this.http.get<User>(this.baseUrl + 'users/' + id);
       }
 
     updateUser(id: number, user: User) {
-      return this.Http.put(this.baseUrl + 'users/' + id, user);
+      return this.http.put(this.baseUrl + 'users/' + id, user);
     }
 
+    setMainPhoto(userId: number, id: number) {
+      return this.http.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain/', {});
+    }
+
+    deletePhoto(userId: number, id: number) {
+      return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
+    }
 }
