f58818eefb493cf8137d3a1fbd5169193759a4dd
kami.jami <kami.jami@gmail.com>
2018-11-30 01:06:27 +0330

    Paging, Sorting and Filtering
    - Paging in ASP.NET Core Web API
    - Adding a PagedList class
    - Setting up the paging helper classes
    - Implementing pagination in the API
    - Setting up pagination in the SPA
    - Using nix-bootstrap pagination module
    - Filtering in the API
    - Adding additional filtering parameters to the API
    - Adding filtering functionality to the SPA
    - Sorting results in the API
    - Adding the Sorting functionality to the SPA

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule, ButtonsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
-import {TimeAgoPipe} from 'time-ago-pipe';
+import { TimeAgoPipe } from 'time-ago-pipe';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -58,7 +58,9 @@ export function tokenGetter() {
       ReactiveFormsModule,
       BsDropdownModule.forRoot(),
       BsDatepickerModule.forRoot(),
+      PaginationModule.forRoot(),
       TabsModule.forRoot(),
+      ButtonsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       NgxGalleryModule,
       FileUploadModule,
